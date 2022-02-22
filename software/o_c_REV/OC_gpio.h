186855ec417b2d6cfe5a4e0160b09a88d8b21f2a
maasijam <50107149+maasijam@users.noreply.github.com>
2022-02-22 22:29:23 +0100

    Update OC_gpio.h

--- software/o_c_REV/OC_gpio.h
+++ software/o_c_REV/OC_gpio.h
@@ -41,14 +41,14 @@
 
 #define DAC_CS 10
 
-#define GATE_1 26
-#define GATE_2 27
-#define GATE_3 28
-#define GATE_4 29
-#define GATE_5 30
-#define GATE_6 31
+#define GATE_1 25
+#define GATE_2 26
+#define GATE_3 27
+#define GATE_4 28
+#define GATE_5 29
+#define GATE_6 30
 
-#define CLKIN 25
+#define CLKIN 31
 
 #ifdef VOR
   #define but_mid 9
